b74f603cb0ce188581dbf6f1fae615d0dc9ed6f6
laufiend <apsaracoefficneou@my.capilanou.ca>
2024-02-09 11:33:58 -0800

    testing php include

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>IDEA Interprets: Danger</title>
+  <meta name="description"
+    content="When writing a meta description, keep it between 140 and 160 characters so Google can display your entire message. Don’t forget to include your keyword!">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta property="og:image" content="screenshot.jpg">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  <script src="https://kit.fontawesome.com/d300c2ea15.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="./styles/style.css">
+</head>
+
+<body class="work-page">
+
+  <section class="hero">
+    <header class="hero-header">
+      <ul class="branding-navigation">
+        <li><a href="artists.php" class="custom-navigation">About the Artists</a></li>
+        <li class="logo"><a href="indextest.php"><img src="assets/branding-images/dangerlogo_icon.svg"/></a></li>
+        <li><a href="about.php" class="custom-navigation">About the Exhibition</a></li>
+      </ul>
+    </header>
---
+++ indextest.php
@@ -0,0 +1,339 @@
+<?php include 'header.php';?>
+   
+   
+   <div class="hero-headline-wrapper">
+     <figure class="hero-image">
+    <img src="assets/branding-images/dangerlogo.svg" alt="IDEA Interprets Danger logo"/>
+  </figure> 
+   
+
+        <h3 class="hero-paragraph">
+          March 7—April 7 2024<br>
+          Wednesday to Sunday, 11am—5pm
+          &nbsp;<br>
+
+          Ferry Building Gallery <br>
+          1414 Argyle Ave <br>
+            West Vancouver <br>
+            </h3>
+      
+
+    </div>
+    <footer class="hero-footer">
+      <ul class="hero-footer-navigation">
+
+      </ul>
+    </footer>
+  </section>
+
+  <main class="work-wrapper">
+    <section class="portfolio">
+      <ul class="grid">
+
+        <li class="work-page-grid-item"><a href="artists.html#jbaik">
+          <figure>
+            <img src="assets/posters/JessicaBaik_temp.jpg"
+              alt="Describe your project">
+          </figure>
+          <div class="overlay">
+            <h1 class="overlay-title">Poster Title</h1>
+            <h4 class="overlay-description">Jessica Baik</h4>
+          </div>
+        </a>
+      </li>
+
+      <li class="work-page-grid-item"><a href="artists.html#sbarry">
+        <figure>
+          <img src="assets/posters/SiobhanBarry_temp.jpg"
+            alt="Describe your project">
+        </figure>
+        <div class="overlay">
+          <h1 class="overlay-title">Bumbly Tumbly</h1>
+          <h4 class="overlay-description">Siobhán Barry</h4>
+        </div>
+      </a>
+    </li>
+
+    <li class="work-page-grid-item"><a href="artists.html#mbrophy">
+      <figure>
+        <img src="assets/posters/MarenBrophy_temp.jpg"
+          alt="Describe your project">
+      </figure>
+      <div class="overlay">
+        <h1 class="overlay-title">Poster Title</h1>
+        <h4 class="overlay-description">Maren Brophy</h4>
+      </div>
+    </a>
+  </li>
+
+  <li class="work-page-grid-item"><a href="artists.html#achau">
+    <figure>
+      <img src="assets/posters/AndyChau_temp.jpg" alt="Describe your project">
+    </figure>
+    <div class="overlay">
+      <h1 class="overlay-title">Doggy Door</h1>
+      <h4 class="overlay-description">Andy Chau</h4>
+    </div>
+  </a>
+</li>
+        <li class="work-page-grid-item">
+          <a href="artists.html#achiang">
+            <figure>
+              <img src="assets/posters/AlexisChiang_temp.jpg"
+                alt="Describe your project">
+            </figure>
+            <div class="overlay">
+              <h1 class="overlay-title">Poster Title</h1>
+              <h4 class="overlay-description">Alexis Chiang</h4>
+            </div>
+          </a>
+        </li>
+
+        <li class="work-page-grid-item"><a href="artists.html#acoefficneou">
+          <figure>
+            <img src="assets/posters/ApsaraCoefficNeou_temp.jpg" alt="Describe your project">
+          </figure>
+          <div class="overlay">
+            <h1 class="overlay-title">Poster Title</h1>
+            <h4 class="overlay-description">Apsara Coeffic-Neou</h4>
+          </div>
+        </a>
+      </li>
+
+      <li class="work-page-grid-item"><a href="artists.html#mdevkota">
+        <figure>
+          <img src="assets/posters/MeshDevkota_temp.jpg"
+            alt="Describe your project">
+        </figure>
+        <div class="overlay">
+          <h1 class="overlay-title">Viral Attack</h1>
+          <h4 class="overlay-description">Mesh Devkota</h4>
+        </div>
+      </a>
+    </li>
+
+    <li class="work-page-grid-item"><a href="artists.html#lgarsonnin">
+      <figure>
+        <img src="assets/posters/LolaGarsonnin_temp.jpg"
+          alt="Describe your project">
+      </figure>
+      <div class="overlay">
+        <h1 class="overlay-title">Poster Title</h1>
+        <h4 class="overlay-description">Lola Garsonnin</h4>
+      </div>
+    </a>
+  </li>
+
+  <li class="work-page-grid-item"><a href="artists.html#bhurt">
+    <figure>
+      <img src="assets/posters/BrendanHurt_temp.jpg"
+        alt="Describe your project">
+    </figure>
+    <div class="overlay">
+      <h1 class="overlay-title">Dangers of Censorship</h1>
+      <h4 class="overlay-description">Brendan Hurt</h4>
+    </div>
+  </a>
+</li>
+
+<li class="work-page-grid-item"><a href="artists.html#jkambashi">
+  <figure>
+    <img src="assets/posters/JerryKambashi_temp.jpg"
+      alt="Describe your project">
+  </figure>
+  <div class="overlay">
+    <h1 class="overlay-title">They Don't Want Us Here</h1>
+    <h4 class="overlay-description">Jerry Kambashi</h4>
+  </div>
+</a>
+</li>
+
+        <li class="work-page-grid-item"><a href="artists.html#akim">
+            <figure>
+              <img src="assets/posters/AnastasiyaKim_temp.jpg" alt="Describe your project">
+            </figure>
+            <div class="overlay">
+              <h1 class="overlay-title">Poster Title</h1>
+              <h4 class="overlay-description">Anastasiya Kim</h4>
+            </div>
+          </a>
+        </li>
+
+        <li class="work-page-grid-item"><a href="artists.html#rlai">
+          <figure>
+            <img src="assets/posters/RaynaLai_temp.jpg"
+              alt="Describe your project">
+          </figure>
+          <div class="overlay">
+            <h1 class="overlay-title">Echoes of the Past</h1>
+            <h4 class="overlay-description">Rayna Lai</h4>
+          </div>
+        </a>
+      </li>
+
+      <li class="work-page-grid-item"><a href="artists.html#jliu">
+        <figure>
+          <img src="assets/posters/JasonLiu_temp.jpg"
+            alt="Describe your project">
+        </figure>
+        <div class="overlay">
+          <h1 class="overlay-title">Poster Title</h1>
+          <h4 class="overlay-description">Jason Liu</h4>
+        </div>
+      </a>
+  </li>
+
+  <li class="work-page-grid-item"><a href="artists.html#rlu">
+    <figure>
+      <img src="assets/posters/RachelLu_temp.jpg"
+        alt="Describe your project">
+    </figure>
+    <div class="overlay">
+      <h1 class="overlay-title">Poster Title</h1>
+      <h4 class="overlay-description">Rachel Lu</h4>
+    </div>
+  </a>
+</li>
+
+<li class="work-page-grid-item"><a href="artists.html#emccammon">
+  <figure>
+    <img src="assets/posters/EvelynMcCammon_temp.jpg"
+      alt="Describe your project">
+  </figure>
+  <div class="overlay">
+    <h1 class="overlay-title">Ice-Cream</h1>
+    <h4 class="overlay-description">Evelyn McCammon</h4>
+  </div>
+</a>
+</li>
+
+<li class="work-page-grid-item"><a href="artists.html#mmcgill">
+  <figure>
+    <img src="assets/posters/AmiliaMcGill_temp.jpg"
+      alt="Describe your project">
+  </figure>
+  <div class="overlay">
+    <h1 class="overlay-title">Poster Title</h1>
+    <h4 class="overlay-description">Mena McGill</h4>
+  </div>
+</a>
+</li>
+
+<li class="work-page-grid-item"><a href="artists.html#span">
+  <figure>
+    <img src="assets/posters/SophiaPan_temp.jpg"
+      alt="Describe your project">
+  </figure>
+  <div class="overlay">
+    <h1 class="overlay-title">Poster Title</h1>
+    <h4 class="overlay-description">Sophia Pan</h4>
+  </div>
+</a>
+</li>
+        <li class="work-page-grid-item"><a href="artists.html#apoystila">
+            <figure>
+              <img src="assets/posters/AndyPoystila_temp.jpg"
+                alt="Describe your project">
+            </figure>
+            <div class="overlay">
+              <h1 class="overlay-title">You in the Zoo!</h1>
+              <h4 class="overlay-description">Andy Poystila</h4>
+            </div>
+          </a>
+        </li>
+
+
+        <li class="work-page-grid-item"><a href="artists.html#kschick">
+          <figure>
+            <img src="assets/posters/KeiraSchick_temp.jpg"
+              alt="Describe your project">
+          </figure>
+          <div class="overlay">
+            <h1 class="overlay-title">Poster Title</h1>
+            <h4 class="overlay-description">Keira Schick</h4>
+          </div>
+        </a>
+      </li>
+
+      <li class="work-page-grid-item"><a href="artists.html#sside">
+        <figure>
+          <img src="assets/posters/ScarlettSide_temp.jpg"
+            alt="Describe your project">
+        </figure>
+        <div class="overlay">
+          <h1 class="overlay-title">Wolf's Regret</h1>
+          <h4 class="overlay-description">Scarlett Side</h4>
+        </div>
+      </a>
+    </li>
+
+    <li class="work-page-grid-item"><a href="artists.html#ttang">
+      <figure>
+        <img src="assets/posters/TriciaTang_temp.jpg"
+          alt="Describe your project">
+      </figure>
+      <div class="overlay">
+        <h1 class="overlay-title">Deuce</h1>
+        <h4 class="overlay-description">Tricia Tang</h4>
+      </div>
+    </a>
+  </li>
+        <li class="work-page-grid-item"><a href="artists.html#cwongpisethkul">
+            <figure>
+              <img src="assets/posters/CharlieWongpsiethkul_temp.jpg"
+                alt="Describe your project">
+            </figure>
+            <div class="overlay">
+              <h1 class="overlay-title">Danger on the Bus</h1>
+              <h4 class="overlay-description">Charlie Wongpisethkul</h4>
+            </div>
+          </a>
+        </li>
+
+        <li class="work-page-grid-item"><a href="artists.html#lyau">
+          <figure>
+            <img src="assets/posters/LouisYau_temp.jpg"
+              alt="Describe your project">
+          </figure>
+          <div class="overlay">
+            <h1 class="overlay-title">Poster Title</h1>
+            <h4 class="overlay-description">Louis Yau</h4>
+          </div>
+        </a>
+      </li>
+
+
+
+    <li class="work-page-grid-item"><a href="artists.html#nyazdani">
+      <figure>
+        <img src="assets/posters/NeekaYazdani_temp.jpg"
+          alt="Describe your project">
+      </figure>
+      <div class="overlay">
+        <h1 class="overlay-title">Poster Title</h1>
+        <h4 class="overlay-description">Neeka Yazdani</h4>
+      </div>
+    </a>
+  </li>
+
+
+
+
+
+
+    <li class="work-page-grid-item"><a href="artists.html#czhang">
+      <figure>
+        <img src="assets/posters/CarolineZhang_temp.jpg" alt="Describe your project">
+      </figure>
+      <div class="overlay">
+        <h1 class="overlay-title">Together Forever</h1>
+        <h4 class="overlay-description">Caroline Zhang</h4>
+      </div>
+    </a>
+  </li>
+
+      </ul>
+    </section>
+  </main>
+
+  <?php include 'footer.php';?>
